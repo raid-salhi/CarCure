c7258e061eca02f7806263f64af2dd2eb8ea1819
raid-salhi <118809948+raid-salhi@users.noreply.github.com>
2024-04-18 16:27:36 +0100

    Set the api interface

--- app/src/main/java/com/example/carcure/network/CarCureApiInterface.kt
+++ app/src/main/java/com/example/carcure/network/CarCureApiInterface.kt
@@ -1,8 +1,17 @@
 package com.example.carcure.network
 
+import com.example.carcure.model.Problem
+import com.example.carcure.model.Sign
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
 import javax.inject.Singleton
 
 @Singleton
 interface CarCureApiInterface {
-
+    @GET("api/signs")
+    suspend fun getSigns ():List<Sign>
+    @POST("api/diagnosis")
+    fun getDiagnosis(@Body signs:List<Sign>): Call<Problem>
 }
