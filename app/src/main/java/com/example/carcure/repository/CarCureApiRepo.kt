92da7aff8f6da1da24fdb95b7ce1dbdb2db305ad
raid-salhi <118809948+raid-salhi@users.noreply.github.com>
2024-04-18 16:32:27 +0100

    added the repo

--- app/src/main/java/com/example/carcure/repository/CarCureApiRepo.kt
+++ app/src/main/java/com/example/carcure/repository/CarCureApiRepo.kt
@@ -1,8 +1,12 @@
 package com.example.carcure.repository
 
+import com.example.carcure.model.Problem
+import com.example.carcure.model.Sign
 import com.example.carcure.network.CarCureApiInterface
+import retrofit2.Call
 import javax.inject.Inject
 
 class CarCureApiRepo @Inject constructor(private val apiInterface: CarCureApiInterface) {
-
+    suspend fun getSigns(): List<Sign> = apiInterface.getSigns()
+    fun getDiagnosis(signs: List<Sign>): Call<Problem> = apiInterface.getDiagnosis(signs)
 }
